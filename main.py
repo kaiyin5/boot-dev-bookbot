0b3b4aa383cd20543723d907245a6b99af188818
kaiyin5 <louiskaiyinng@gmail.com>
2025-09-19 23:48:28 +0800

    handling file path error

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import sys
 
 def main():
     # handling user argument
-    try:
-        if len(sys.argv) != 2:
-            print("Usage: python3 main.py <path_to_book>")
-            sys.exit(1)
-        file_path = sys.argv[1]
-    except Exception as e:
-        print(e)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
+    file_path = sys.argv[1]
+    
 
     # getting data
-    book_text = get_book_text(file_path)
+    try:
+        book_text = get_book_text(file_path)
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(2)
     num_words = get_num_words(book_text)
     num_chars = get_num_chars(book_text)
     char_num_pair = get_char_num_pair(num_chars)
